6dd1a1f586e1794882b7ec5eb7f33a61cce93230
Nikola Stojiljkovic <nikolavla@gmail.com>
2015-06-09 23:50:31 +0200

    Almost finished update parent after job is taken

---
+++ src/main/java/utils/HashTableValueUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+import protos.KademliaProtos.HashTableValue;
+
+public class HashTableValueUtils {
+
+	public static HashTableValue updateParent(HashTableValue parentValue,
+			HashTableValue leftChildValue, HashTableValue rightChildValue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/utils/StatisticsUtils.java
@@ -0,0 +1,45 @@
+package utils;
+
+import protos.KademliaProtos.HashTableValue;
+import util.Constants;
+
+public class StatisticsUtils {
+
+	public static boolean isAllFinished(HashTableValue value) {
+		return value.getFinishedTasks() == value.getValidTasks();
+	}
+
+	public static boolean isLeafNode(HashTableValue value) {
+		return value.hasUnitTask();
+	}
+	
+	public static boolean isAllInProgress(HashTableValue value) {
+		long currentTime = System.currentTimeMillis();
+		long lastTimeTaken = value.getLastTimeTaken();
+		return currentTime - lastTimeTaken < Constants.JOB_TIMEOUT_TIME_MS;
+	}
+	
+	public static boolean isNonTakenTask(HashTableValue value) {
+		if (!isLeafNode(value)) {
+			return false;
+		}
+		
+		if (isAllInProgress(value)) {
+			return false;
+		}
+		
+		if (isAllFinished(value)) {
+			return false;
+		}
+		
+		return true;
+	}
+
+	public static boolean hasNonFinishedTasksNotInProgress(HashTableValue value) {		
+		if (isAllFinished(value) || isAllInProgress(value)) {
+			return false;
+		}		
+		return true;
+	}
+
+}
